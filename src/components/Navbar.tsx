109666e2346e97ed867d7e2e3a3ea1d51ab9eaf5
William git config --global user.mail <w.tousson@ecole-ipssi.net>
2025-06-21 01:55:21 +0200

    [UPDATE] Modif_NAVBAR

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { Link, useLocation } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
   const location = useLocation();
 
   const navItems = [
@@ -14,10 +15,31 @@ const Navbar: React.FC = () => {
     { label: "Entreprise", path: "../entreprises" },
     { label: "Tableau de synthèse", path: "../tableau-synthese" },
   ];
+  
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+      lastScrollY = window.scrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <>
-      <nav className="w-full h-[100px] flex items-center justify-between px-10 fixed top-0 z-50 bg-transparent font-ubuntu">
+      <nav
+        className={`w-full h-[100px] flex items-center justify-between px-10 fixed top-0 z-50 bg-transparent font-ubuntu transition-transform duration-500 ${
+          isVisible ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
         <div className="text-white text-2xl font-bold"></div>
         <div className="hidden md:flex space-x-10 text-white text-xl">
           {navItems.map(({ label, path }, index) => {
@@ -32,8 +54,7 @@ const Navbar: React.FC = () => {
                 <span
                   className={`absolute left-0 bottom-0 h-0.5 bg-white transition-all duration-500
                   ${isActive ? "w-full" : "w-0"}
-                  group-hover:w-full
-                `}
+                  group-hover:w-full`}
                 />
               </Link>
             );
@@ -46,9 +67,11 @@ const Navbar: React.FC = () => {
         >
           {isOpen ? <FaTimes /> : <FaBars />}
         </button>
+
         <div
-          className={`fixed top-0 right-0 h-full w-[70%] bg-black/90 text-white flex flex-col items-center justify-center space-y-6 text-2xl transition-transform duration-500 ${isOpen ? "translate-x-0" : "translate-x-full"
-            } md:hidden`}
+          className={`fixed top-0 right-0 h-full w-[70%] bg-black/90 text-white flex flex-col items-center justify-center space-y-6 text-2xl transition-transform duration-500 ${
+            isOpen ? "translate-x-0" : "translate-x-full"
+          } md:hidden`}
         >
           {navItems.map(({ label, path }, index) => {
             const isActive = location.hash === path;
@@ -56,8 +79,9 @@ const Navbar: React.FC = () => {
               <Link
                 key={index}
                 to={path}
-                className={`hover:text-violet-500 text-white ${isActive ? "border-b-2 border-white" : ""
-                  }`}
+                className={`hover:text-violet-500 text-white ${
+                  isActive ? "border-b-2 border-white" : ""
+                }`}
                 onClick={() => setIsOpen(false)}
               >
                 {label}
@@ -65,6 +89,7 @@ const Navbar: React.FC = () => {
             );
           })}
         </div>
+
         <div
           className="hidden md:block profil w-12 h-12 rounded-full overflow-hidden cursor-pointer border-2 border-white"
           onClick={() => setIsModalOpen(true)}
