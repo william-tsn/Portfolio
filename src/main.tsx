d860c357c74c87170ee2588744f6491929629a8e
William git config --global user.mail <w.tousson@ecole-ipssi.net>
2025-06-20 21:12:44 +0200

    [CORRECTION]_modif+bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.tsx'
-import { HashRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
